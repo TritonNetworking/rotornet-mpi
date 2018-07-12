8aaea6ab1b3717f078e3073918beafef94cb83b5
Yibo Guo <nil.yibo@gmail.com>
2018-07-12 16:19:20 -0700

    Added RDMA and MPI work as of 07/12/2018.
    Commit: a1261c03577edf9522d3a4208fc525b5d1eac756
    Repo: git@bitbucket.org:nilyibo/dc-circuitswitch.git

---
+++ sysnet-benchmarks/script/build.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# Check if MPI environment is loaded
+if ! [ -x "$(command -v mpirun)" ]; then
+    source ./setup-hpcx.sh
+fi
+
+# Parse command line arguments
+# Source: https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -f|--force)
+    FORCE=true
+    shift # past argument
+    ;;
+    -c|--clean)
+    CLEAN=true
+    shift # past argument
+    ;;
+    -d|--debug)
+    DEBUG=true
+    shift # past argument
+    ;;
+    *)
+    echo "Unrecognized option $key"
+    exit 2
+esac
+done
+
+(#set -x;
+mkdir -p ../build
+cd ../build
+
+if [ $FORCE ]; then
+    rm -rf *
+    if [ $DEBUG ]; then
+        cmake -DCMAKE_BUILD_TYPE=Debug ../src
+    else
+        cmake -DCMAKE_BUILD_TYPE=Release ../src
+    fi
+fi
+
+if [ $CLEAN ]; then
+    make clean
+fi
+
+make
+)
+
---
+++ sysnet-benchmarks/script/run_all_length.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [[ $# -gt 2 ]]; then
+    echo "$0 [<server ip>]"
+fi
+
+l=1
+limit=8388608
+
+count=1000
+verb="write"
+mode="throughput"
+warmup=0
+mr_count=1
+
+server="$1"
+
+cd ../build
+while [[ $l -le $limit ]]; do
+    echo "Length = $l ..."
+    ./rdma_exec -b $l -r $count -v $verb -m $mode -w $warmup --mr_count=$mr_count $server
+    (( l *= 2 ))
+    echo ""
+done
+
